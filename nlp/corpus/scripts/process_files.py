2eaabfe80f7abf2ea5689e7fcf7eff488c6485dc
Stiven Cardona Monsalve <stiven.cardona627@gmail.com>
2021-06-26 15:50:42 -0500

    update corpus to first nlp taller

---
+++ nlp/corpus/scripts/process_files.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+filename_list = ['dialogueText']  # ['dialogueText_196','dialogueText_301']
+
+
+def add_record(files_dict, filename, record):
+    files_dict[filename].append(
+        f'{record["dialogueID"]},{record["date"]},{record["from"]},{record["to"]},\"{record["text"]}\"\n')
+
+
+def segregate_data_by_custom_field(name_field):
+    df = pd.read_csv(f'../{filename_list[0]}.csv')
+    files_dict = dict()
+
+    for index, row in df.iterrows():
+        filename = row[name_field]
+        if not filename in files_dict:
+            files_dict[filename] = []
+        add_record(files_dict, filename, row)
+
+    headers = 'dialogueID,date,from,to,text\n'
+    for key in files_dict:
+        lines = files_dict[key]
+        f = open(f'./output/{key}.csv', 'w', encoding='utf-8')
+        f.write(headers)
+        f.writelines(lines)
+        f.close()
+
+
+segregate_data_by_custom_field('from')
